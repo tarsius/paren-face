7117d73a4d006be8a1e6730bb167d691e284c78b
Jonas Bernoulli <jonas@bernoul.li>
2022-04-22 18:27:02 +0200

    Refresh library header

--- paren-face.el
+++ paren-face.el
@@ -1,26 +1,27 @@
-;;; paren-face.el --- a face for parentheses in lisp modes  -*- lexical-binding: t -*-
+;;; paren-face.el --- A face for parentheses in lisp modes  -*- lexical-binding:t -*-
 
-;; Copyright (C) 2013-2022  Jonas Bernoulli
+;; Copyright (C) 2013-2022 Jonas Bernoulli
 
-;; Maintainer: Jonas Bernoulli <jonas@bernoul.li>
+;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/tarsius/paren-face
+;; Keywords: faces lisp
 
 ;; Package-Requires: ((emacs "25.1") (compat "28.1.1.0"))
 
-;; This file is not part of GNU Emacs.
+;; SPDX-License-Identifier: GPL-3.0-or-later
 
-;; This file is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
-
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation, either version 3 of the License,
+;; or (at your option) any later version.
+;;
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
-;; For a full copy of the GNU General Public License
-;; see <http://www.gnu.org/licenses/>.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
