d36fab3529aa3e046493ec373f2c080874cc184f
Jonas Bernoulli <jonas@bernoul.li>
2024-01-01 23:24:46 +0100

    Bump copyright years

--- paren-face.el
+++ paren-face.el
@@ -1,6 +1,6 @@
 ;;; paren-face.el --- A face for parentheses in lisp modes  -*- lexical-binding:t -*-
 
-;; Copyright (C) 2013-2023 Jonas Bernoulli
+;; Copyright (C) 2013-2024 Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/tarsius/paren-face
