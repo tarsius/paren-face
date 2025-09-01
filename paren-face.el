ccbf5f5a61f469e40bc7971e6b033d1ee35bf14d
Jonas Bernoulli <jonas@bernoul.li>
2025-09-01 20:03:24 +0200

    Release version 1.2.1

--- paren-face.el
+++ paren-face.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/tarsius/paren-face
 ;; Keywords: faces lisp
 
-;; Package-Version: 1.2.0
+;; Package-Version: 1.2.1
 ;; Package-Requires: ((emacs "26.1") (compat "30.1"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
